be8c1c9d92a15bdf4fef8bce44f34e4d71138efe
bene2510 <bene2510@edu.ucl.dk>
2023-02-19 13:56:06 +0100

    Mere JS

--- logind.js
+++ logind.js
@@ -1,14 +1,14 @@
 console.log("JS fungerer perfekt jo!");
 
 let myInput = document.getElementById("password");
-let tal = document.getElementById("tal");
-let karakterer = document.getElementById("karakterer");
+let numbers = document.getElementById("numbers");
+let characters = document.getElementById("characters");
 let mySubmit = document.getElementById ("submit");
-let taltal = /[0-9]/g;
+let numberstonine = /[0-9]/g;
 let myForgot = document.getElementById("glemtkode");
 let myUsername = document.getElementById("username");
-let bogstaver = /[a-z]/g;
-let bogbog = document.getElementById("bogstaver");
+let smallletters = /[a-z]/g;
+let smallletterstwo = document.getElementById("smallletterstwo");
 
 myInput.onfocus = function() {
     document.getElementById("passwordmessage").style.display = "block";
@@ -25,38 +25,38 @@ myUsername.onblur = function() {
 }
 
 myUsername.onkeyup = function() {
-  if(myUsername.value.match(bogstaver)) {
-    bogbog.classList.remove("invalid");
-    bogbog.classList.add("valid");
+  if(myUsername.value.match(smallletters)) {
+    smallletterstwo.classList.remove("invalid");
+    smallletterstwo.classList.add("valid");
   }
   else {
-    bogbog.classList.remove("valid");
-    bogbog.classList.add("invalid");
+    smallletterstwo.classList.remove("valid");
+    smallletterstwo.classList.add("invalid");
   }
 }
 
 myInput.onkeyup = function() {
-  if(myInput.value.match(taltal)) {  
-    tal.classList.remove("invalid");
-    tal.classList.add("valid");
+  if(myInput.value.match(numberstonine)) {  
+    numbers.classList.remove("invalid");
+    numbers.classList.add("valid");
   } else {
-    tal.classList.remove("valid");
-    tal.classList.add("invalid");
+    numbers.classList.remove("valid");
+    numbers.classList.add("invalid");
   }
 
   if(myInput.value.length >= 8) {
-    karakterer.classList.remove("invalid");
-    karakterer.classList.add("valid");
+    characters.classList.remove("invalid");
+    characters.classList.add("valid");
   } else {
-    karakterer.classList.remove("valid");
-    karakterer.classList.add("invalid");
+    characters.classList.remove("valid");
+    characters.classList.add("invalid");
   }
 }
 
 let besked = ["Log ind godkendt","Forkert adgangskode eller brugernavn","Glemt kode eller brugernavn?"]
 
 mySubmit.onclick = function() {
-  if(myInput.value.match(taltal) && myInput.value.length >= 8 && myUsername.value.match(bogstaver)) {
+  if(myInput.value.match(numberstonine) && myInput.value.length >= 8 && myUsername.value.match(smallletters)) {
     alert(besked[0]);
   }
   else {
